e53e11606c08b27bbcefb8a7133556fe013bc26b
James Spears <jamesspearsv@gmail.com>
2023-11-17 07:32:17 -0500

    Finished webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/scripts/index.js",
+  entry: "./src/scripts/main.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
+    // Static content loaders
     rules: [
       {
         test: /\.css$/i,
-
         use: ["style-loader", "css-loader"],
       },
       {
