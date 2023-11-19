b3437517319ca8ad2521f5f001a9396b527b5665
James Spears <jamesspearsv@gmail.com>
2023-11-19 17:36:40 -0500

    Began UI design and build

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@
 // [x] list controller
 // [ ] view controller
 
+import "./styles/style.css";
+
 import { ListController } from "./components/listController";
 
 window.ListController = ListController;
